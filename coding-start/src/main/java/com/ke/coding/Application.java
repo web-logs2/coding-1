e5aae7cc965662a859f17c164004eab7c7fb4318
xueyunlong001 <xueyunlong001@ke.com>
2023-03-17 09:55:19 +0800

    [需求][50282459]风控中台2月份事务性需求

--- coding-start/src/main/java/com/ke/coding/Application.java
+++ coding-start/src/main/java/com/ke/coding/Application.java
@@ -23,7 +23,7 @@ public class Application implements CommandLineRunner {
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
+	public void run(String... args) {
 		resolver.run();
 	}
 }
